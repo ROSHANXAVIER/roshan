46060c423e4903ce61f54b9a0845feb1d305c32b
ROSHAN XAVIER <84636207+ROSHANXAVIER@users.noreply.github.com>
2022-06-06 18:37:06 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ function Navbar() {
   return (
       <div>
     <header>
-  <h1>ZOLA</h1>
+  <h1>ROSHAN WEBSITE</h1>
   
 </header>
 <div className='pos'><button className='button-17'>LOGIN</button><button className='button-17'>REGISTER</button></div>
